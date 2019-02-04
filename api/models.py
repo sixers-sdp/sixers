ed373a0c9d44ee3b4a47593b787a868340de771d
Visgean Skeloru <me@visgean.me>
2019-02-04 19:44:24 +0000

    add prototypes of some of the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+
+from .app import db
 
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
 
+    def __repr__(self):
+        return self.name
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    products = db.relationship('Product', backref='orders', lazy=False)
+
+    def __repr__(self):
+        return self.created
