baf156b0cc5ecbc39c821c4c3377e39d1bdb0b04
Visgean Skeloru <me@visgean.me>
2019-02-05 01:59:19 +0000

    add basic models for the app

---
+++ api/main/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+ORDER_STATE_NEW = 'new'
+ORDER_STATE_DELIVERY = 'delivery'
+ORDER_STATE_FINISHED = 'finished'
+ORDER_STATE_ABORTED = 'aborted'
+
+ORDER_STATE_CHOICES = [
+    (s, s)
+    for s in [ORDER_STATE_NEW, ORDER_STATE_DELIVERY, ORDER_STATE_FINISHED, ORDER_STATE_ABORTED]
+]
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __repr__(self):
+        return self.name
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    table_number = models.IntegerField()
+
+    products = models.ManyToManyField(Product)
+    state = models.CharField(
+        choices=ORDER_STATE_CHOICES,
+        max_length=24
+    )
+
