8da103c91551f7a5adbdd82374fe873ffc04521a
Visgean Skeloru <me@visgean.me>
2019-02-06 19:35:29 +0000

    add production settings

---
+++ api/api/production_settings.py
@@ -0,0 +1,22 @@
+# Normally I would have used env level settings but this is just prototype....
+
+from .default_settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '188.166.173.237',
+    'albert.visgean.me',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
