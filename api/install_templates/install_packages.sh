f841a820abc61038dcc6934248edeec7a89ae609
Visgean Skeloru <me@visgean.me>
2019-02-11 21:59:32 +0000

    map using networkx

--- api/install_templates/install_packages.sh
+++ api/install_templates/install_packages.sh
@@ -2,3 +2,5 @@
 
 sudo apt-get update
 sudo apt-get install python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx
+
+sudo apt-get install graphviz libgraphviz-dev pkg-config
---
+++ map/map.py
@@ -0,0 +1,30 @@
+import networkx as nx
+from networkx.drawing.nx_pydot import write_dot
+
+
+g = nx.Graph()
+
+# add tables:
+g.add_nodes_from(['t1', 't2', 't3', 't4'])
+
+# add chef:
+g.add_nodes_from(['chef'])
+
+# add connecting nodes:
+
+g.add_nodes_from(['cross1', 'cross2', 'cross3'])
+
+g.add_edge('chef', 'cross1', color='red')
+g.add_edge('cross1', 'cross2')
+g.add_edge('cross2', 't3')
+g.add_edge('cross2', 't4')
+
+g.add_edge('cross2', 'cross3')
+g.add_edge('cross3', 't1')
+g.add_edge('cross3', 't2')
+
+
+if __name__ == '__main__':
+    pos = nx.nx_agraph.graphviz_layout(g)
+    nx.draw(g, pos=pos)
+    write_dot(g, 'generated.dot')
