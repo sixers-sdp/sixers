9862dec432cc868e1a6581357e1d0304d2019883
Visgean Skeloru <me@visgean.me>
2019-02-25 15:14:42 +0000

    extend the simple map

--- api/map/simple_map.py
+++ api/map/simple_map.py
@@ -4,7 +4,7 @@ from networkx.drawing.nx_pydot import write_dot
 
 current_map = nx.Graph()
 
-tables = ['t1']
+tables = ['t1', 't2', 't3']
 CHEF = 'chef'
 
 # add tables:
@@ -15,3 +15,5 @@ current_map.add_nodes_from([CHEF])
 
 
 current_map.add_edge('chef', 't1', color='red')
+current_map.add_edge('t1', 't2', color='blue')
+current_map.add_edge('chef', 't3', color='red')
