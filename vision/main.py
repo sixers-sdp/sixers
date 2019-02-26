34ac16a4400a7cb31e3d44c1678115b10169c4a1
s1709221 <s1709221@ed.ac.uk>
2019-02-26 19:52:47 +0000

    new vision stuff

--- vision/main.py
+++ vision/main.py
@@ -1,51 +1,25 @@
 import cv2
 import numpy as np
 import time as global_time
-
 from threading import Thread
+import json
+from motor_board import start_specific_motor, stop_specific_motor, stop_all_motors
 
-from motor_board import (
-    start_specific_motor, stop_specific_motor,
-    stop_all_motors
-)
+def enum(*args):
+    enums = dict(zip(args, range(len(args))))
+    return type('Enum', (), enums)
 
-
-# Constants:
-
-NUM_GRIDS = 10
-HUE_THRESHOLD = 10
-SATURATION_THRESHOLD = 20
-
-HUE = 170
-SATURATION = 80
-RED_THRESHOLD = 200
-
-FRAME_SLEEP = 0.1
-
-# Global variable to hold the current captured screen from the cam
-camera = {"frame": None, "thread": None}
-
-
-def start_camera(cam):
-    """
-    This will run in a separate thread and update the  global variable `camera`
-    """
-
-    global camera
-    while True:
-        camera["frame"] = cam.read()[1]
-
+Directions = enum('LEFT', 'RIGHT', 'STRAIGHT')
 
 class Motor:
-    def __init__(self, motor_id):
-        self.motor_id = motor_id
+    def __init__(self, id):
+        self.motor_id=id
 
     def forward(self, speed=100):
         start_specific_motor(self.motor_id, speed)
 
     def backward(self, speed=-100):
-        if speed > 0:
-            speed = -1 * speed
+        if(speed>0): speed=-1*speed
         start_specific_motor(self.motor_id, speed)
 
     def stop(self):
@@ -57,184 +31,250 @@ class AlbertMotion:
         self.motor_1 = Motor(id_1)
         self.motor_2 = Motor(id_2)
 
-    def forward(self, speed=80, time=0.25):
+    def forward(self, speed=100, time=0.30):
         self.motor_1.forward(speed)
         self.motor_2.forward(speed)
         global_time.sleep(time)
-        self.stop()
+	#self.motor_1.stop()
+        #self.stop()
 
-    def backward(self, speed=-80, time=0.35):
-        if speed > 0:
-            speed = -1 * speed
+    def backward(self, speed=-100, time=0.30):
+        if(speed>0): speed=-1*speed
         self.motor_1.backward(speed)
         self.motor_2.backward(speed)
         global_time.sleep(time)
-        self.stop()
+        #self.stop()
 
-    def turn_left(self, speed=80, time=0.5, turn_in_place=False):
-        if turn_in_place:
-            self.motor_1.backward(speed)
-        self.motor_2.forward(speed)
-        global_time.sleep(time)
-        self.stop()
+    def turn_left(self, speed=100, time=0.5, turn_in_place=False):
+    	if(turn_in_place):
+    	    self.motor_1.backward(speed)
+    	self.motor_2.forward(speed)
+    	global_time.sleep(time)
+        #self.stop()
 
-    def turn_right(self, speed=80, time=0.5, turn_in_place=False):
-        self.motor_1.forward(speed)
-        if turn_in_place:
-            self.motor_2.backward(speed)
-        global_time.sleep(time)
-        self.stop()
+    def turn_right(self, speed=100, time=0.5, turn_in_place=False):
+    	self.motor_1.forward(speed)
+    	if(turn_in_place):
+    	    self.motor_2.backward(speed)
+    	global_time.sleep(time)
+    	#self.motor_1.forward(speed)
+        #self.stop()
 
     def stop(self):
         stop_all_motors()
 
-
 class Albert:
     def __init__(self):
-        self.motion = AlbertMotion(4, 2)
+	self.motion = AlbertMotion(4,2)
 
-    def connect_to_vision(self):
-        global camera
-        h = 480
-        w = 640
-        top_left = 1
-        bottom_left = 0
-        top_right = 1
-        bottom_right = 0
+camera={"frame": None, "thread": None, "end": False}
 
-        # Fixme: remove redundant variables
-        left_power = 0
-        right_power = 0
+def start_camera(cam):
+    global camera
+    while not camera["end"]:
+        #start_time = global_time.time()
+        camera["frame"] = cam.read()[1]
+        #print(global_time.time()-start_time)
 
-        good_angle = 25
-        good_angle_error = 3
-        none_found = False
 
-        angle_error = 15
+def connect_to_vision(albert):
+    global camera
 
-        to_turn = 0
-        c = 175
+    NUM_GRIDS = 31
 
-        # cv2.namedWindow("preview")
-        vc = cv2.VideoCapture(0)
-        camera["thread"] = Thread(target=start_camera, args=(vc,))
-        camera["thread"].daemon = True
-        camera["thread"].start()
 
-        # if vc.isOpened(): # try to get the first frame
-        #    rval, frame = vc.read()
-        # else:
-        #    rval = False
+    saw_corner = False
+    directions = [1, 1, 0]
 
-        while True:
-            prev = global_time.time()
-            # cv2.imshow("preview", frame)
-            frame = camera["frame"]
-            if frame is None:
-                continue
 
-            key = cv2.waitKey(1)
-            if key == 27:  # exit on ESC
-                break
+    #NUM_GRIDS_i = NUM_GRIDS
+    #NUM_GRIDS_j = int(round(NUM_GRIDS*.75))
 
-            # print("Frame Rate: "+str(global_time.time()-prev))
+    NUM_GRIDS_i = 640
+    NUM_GRIDS_j =480
 
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    horz_best = int(NUM_GRIDS_i/2)
+    vert_best = NUM_GRIDS_j
 
-            pink_min = np.array([160, 50, 50], np.uint8)
-            pink_max = np.array([180, 255, 255], np.uint8)
+    HUE_THRESHOLD = 10
+    SATURATION_THRESHOLD = 20
+    h=480
+    w=640
+    HUE = 170
+    SATURATION = 80
+    RED_THRESHOLD = 200
+    top_left = 1
+    bottom_left = 0
+    top_right = 1
+    bottom_right = 0
 
-            frame_threshed = cv2.inRange(hsv, pink_min, pink_max)
+    FRAME_SLEEP=0.1
 
-            frame = frame_threshed
+    left_power = 0
+    right_power = 0
 
-            const = len(frame) // NUM_GRIDS  # Fixme: what is this?
+    good_angle = 40
+    good_angle_error = 3
+    none_found = False
 
-            grid = np.zeros((NUM_GRIDS, h / NUM_GRIDS, w))
-            for i in range(0, NUM_GRIDS):
-                grid[i] = frame[const * i:const * (i + 1)]
+    angle_error = 15
 
-            # Fixme: what does this code do?
-            for i in range(grid.shape[0] - 1, grid.shape[0]):
-                none_found = False
-                try:
-                    top_left = np.argwhere(grid[i, 0, :] == 255)[0][0]
-                    # cv2.rectangle(frame,(top_left-20,i*grid.shape[1]),(top_left+20,i*grid.shape[1]+20),(255,255,255),3)
-                except:
-                    # print("Yolo")
-                    none_found = True
+    to_turn = 0
+    c = 160
+    ALLOW = 10
 
-                try:
-                    bottom_left = np.argwhere(grid[i, -1, :] == 255)[0][0]
-                    # cv2.rectangle(frame,(bottom_left-20,(i+1)*grid.shape[1]),(bottom_left+20,(i+1)*grid.shape[1]-20),(255,255,255),3)
+    #cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+    camera["thread"] = Thread(target=start_camera, args=(vc, ))
+    camera["thread"].daemon = True
+    camera["thread"].start()
 
-                except:
-                    pass
+    #if vc.isOpened(): # try to get the first frame
+    #    rval, frame = vc.read()
+    #else:
+    #    rval = False
 
-                try:
-                    top_right = np.argwhere(grid[i, 0, :] == 255)[-1][0]
-                    # cv2.rectangle(frame,(top_right-20,i*grid.shape[1]),(top_right+20,i*grid.shape[1]+20),(255,255,255),3)
-                except:
-                    # print("Yolo")
-                    none_found = True
+    running_index = 0
+    i=0
+    while True:
+        prev = global_time.time()
+        frame = camera["frame"]
+        if(frame is None): continue
+        key = cv2.waitKey(1)
+        if key == 27: # exit on ESC
+            break
 
-                try:
-                    bottom_right = np.argwhere(grid[i, -1, :] == 255)[-1][0]
-                    # cv2.rectangle(frame,(bottom_right-20,(i+1)*grid.shape[1]-1),(bottom_right+20,(i+1)*grid.shape[1]-20),(255,255,255),3)
+        #print("Frame Rate: "+str(global_time.time()-prev))
 
-                except:
-                    pass
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-                # global_time.sleep(FRAME_SLEEP)
-                # print("Before Conditioning: "+str(global_time.time()-prev))
-                # print()
+        PINK_MIN = np.array([0, 50, 50],np.uint8)
+        PINK_MAX = np.array([4, 255, 255],np.uint8)
 
-                if i == grid.shape[0] - 1:
-                    a = top_left - bottom_left
-                    if a == 0:
-                        a = 0.00000001
+        frame_threshed = cv2.inRange(hsv, PINK_MIN, PINK_MAX)
 
-                    b = top_right - bottom_right
-                    if b == 0:
-                        b = 0.00000001
-                    left_angle = np.degrees(np.arctan(float(grid.shape[1]) / a))
-                    left_angle = np.sign(left_angle) * (90 - np.abs(left_angle))
+	kernel = np.ones((5,5),np.uint8)
+    	frame_threshed = cv2.dilate(frame_threshed,kernel,iterations=10)
 
-                    right_angle = np.degrees(np.arctan(float(grid.shape[1]) / b))
-                    right_angle = np.sign(right_angle) * (90 - np.abs(right_angle))
+        frame = frame_threshed
 
-                    # print("Before Action: "+str(global_time.time()-prev))
+        #CONST_i = w//NUM_GRIDS_i
+        #CONST_j = h//NUM_GRIDS_j
 
-                    if none_found:
-                        self.motion.stop()
-                        print("Stop")
-                    # elif ((bottom_left>=((w/2)-c)) and (bottom_right<=((w/2)+c))):
-                    elif np.abs(left_angle + right_angle) < good_angle:
-                        if bottom_left < ((w / 2) - c):
-                            print("Forwards (slight right)")
-                            self.motion.turn_left(time=0.25, turn_in_place=True)
-                        elif bottom_right > ((w / 2) + c):
-                            print("Forwards (slight left)")
-                            self.motion.turn_right(time=0.25, turn_in_place=True)
-                        else:
-                            print("Forwards")
-                            self.motion.forward()
-                    else:
-                        print("Turn")
-                        to_turn = int((left_angle + right_angle) / 2)
-                        if np.sign(to_turn) > 0:
-                            self.motion.turn_right(time=0.4, turn_in_place=False)
-                        else:
-                            self.motion.turn_left(time=0.4, turn_in_place=False)
-            # print("Entire loop: "+str(global_time.time()-prev))
+        #grid = np.zeros((NUM_GRIDS_i,NUM_GRIDS_j,int(h/NUM_GRIDS_j),int(w/NUM_GRIDS_i)))
+        #grid = np.zeros((NUM_GRIDS_i,NUM_GRIDS_j))
 
-        cv2.destroyWindow("preview")
-        vc.release()
+        #horz_list = np.zeros(NUM_GRIDS_i)
+        #vert_list = np.zeros(NUM_GRIDS_j)
 
+        #for i in range(0, NUM_GRIDS_i):
+        #    for j in range(0, NUM_GRIDS_j):
+        #        grid[i,j] = np.sum(frame[CONST_j*j:CONST_j*(j+1),CONST_i*i:CONST_i*(i+1)])
+
+
+        #for i in range(NUM_GRIDS_i):
+        #    horz_list[i] = np.sum(frame[i,:])
+
+        #for j in range(NUM_GRIDS_j):
+        #    vert_list[j] = np.sum(frame[:,j])
+
+        horz_list =  frame.sum(axis=0)
+        vert_list = frame.sum(axis=1)
+
+        horz_idx = np.argmax(horz_list)+1
+        vert_idx = np.argmax(vert_list)+1
+
+        horz_diff = horz_best-horz_idx
+        vert_diff = vert_best-vert_idx
+
+        print(h-vert_idx)
+        #saw_corner = True
+
+
+        if i < 5:
+	    i+=1
+            continue
+        print(global_time.time() - prev)
+	#continue
+        #if(len(directions) == 0):
+        #    print("Order Complete!")
+        #    global_time.sleep(1)
+        #    continue
+
+        if (not saw_corner and h - vert_idx > 150):
+            albert.motion.stop()
+            saw_corner = True
+            albert.motion.stop()
+            global_time.sleep(0.2)
+	    albert.motion.forward(time=0.3)
+            global_time.sleep(0.2)
+            albert.motion.stop()
+            global_time.sleep(0.4)
+            print("Stop! Corner!")
+        elif saw_corner and h - vert_idx <= 150:
+            saw_corner = False
+            directions = directions[1:]
+            print("Corner Escaped")
+
+	if (saw_corner):
+            direction = directions[0]
+            if direction == Directions.LEFT:
+                #albert.motion.forward(time=0.25)
+                #albert.motion.stop()
+                #global_time.sleep(0.4)
+                albert.motion.turn_left(time=0.4, turn_in_place=True)
+                global_time.sleep(0.25)
+                albert.motion.stop()
+                global_time.sleep(0.4)
+                print("CORNER LEFT")
+            elif direction == Directions.RIGHT:
+                #albert.motion.forward(time=0.25)
+                #albert.motion.stop()
+                #global_time.sleep(0.4)
+                albert.motion.turn_right(time=0.4, turn_in_place=True)
+                global_time.sleep(0.25)
+                albert.motion.stop()
+                global_time.sleep(0.4)
+                print("CORNER RIGHT")
+            else:
+                albert.motion.forward(time=0.1)
+                global_time.sleep(0.1)
+                albert.motion.stop()
+                global_time.sleep(0.4)
+                print("CORNER STRAIGHT")
+            continue
+        else:
+            if (np.abs(w//2 - horz_idx)>50):
+                if (w//2 - horz_idx < 0):
+                    print("Slight Right")
+                    albert.motion.turn_right(time=0.25,turn_in_place=True)
+                else:
+                    print("Slight Left")
+                    albert.motion.turn_left(time=0.25,turn_in_place=True)
+            else:
+                albert.motion.forward(time=0.3)
+                print("Forwards")
+
+	running_index += 1
+        if not saw_corner and running_index > 0:
+	    albert.motion.stop()
+            print("Stopping to optimise speed")
+            global_time.sleep(0.05)
+            running_index = 0
+        #print(global_time.time()-prev)
+
+    cv2.destroyWindow("preview")
+    vc.release()
+
+
+def main():
+     global camera
+     try:
+        albert = Albert()
+        connect_to_vision(albert)
+     except(KeyboardInterrupt):
+        albert.motion.stop()
+        camera["end"]=True
 
 if __name__ == '__main__':
-    albert = Albert()
-    try:
-        albert.connect_to_vision()
-    except KeyboardInterrupt:
-        albert.motion.stop()
+     main()
