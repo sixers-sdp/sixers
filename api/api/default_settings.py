7adc98118281c5b1609f13c1fd1ae4a67d1f5eb3
Visgean Skeloru <me@visgean.me>
2019-03-05 21:12:27 +0000

    enable readonly mode for api

--- api/api/default_settings.py
+++ api/api/default_settings.py
@@ -141,6 +141,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
 }
