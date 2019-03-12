0c399237727dd02afcdb344997e33e26bb3f5192
Visgean Skeloru <me@visgean.me>
2019-03-12 20:56:21 +0000

    associate table numbers by dot device

--- api/albert_api/serializers.py
+++ api/albert_api/serializers.py
@@ -11,16 +11,23 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     # products = ProductSerializer()
+    device_id = serializers.CharField(write_only=True)
 
     class Meta:
         model = Order
-        exclude = []
+        fields = ['products_text', 'table_number', 'products', 'device_id', 'state']
+        # write_only_fields = ['device_id']
 
         extra_kwargs = {
             'table_number': {'read_only': True},
         }
 
-
+    def create(self, validated_data):
+        device_id = validated_data.pop('device_id')
+        table_num = DotAssociation.objects.get(dot_id=device_id).location
+        validated_data['table_number'] = table_num
+        order = super().create(validated_data)
+        return order
 
 class PlanSerializer(serializers.ModelSerializer):
     steps = serializers.JSONField(source='plan_as_json', read_only=True)
