b670665fb27f0732ff331ac78005fabdc443a0ef
Visgean Skeloru <me@visgean.me>
2019-03-29 17:17:09 +0000

    fix up deployment script

--- main/deploy.sh
+++ main/deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Pulling latest repo on EV3"
-ssh ev3 bash -c "cd sixers;git pull"
+ssh ev3 "cd sixers;git pull"
 
 echo "Restarting ev3 systemd service"
 
-ssh ev3_root bash -c "systemctl daemon-reload"
-ssh ev3_root bash -c "systemctl restart ev3.service"
+ssh ev3_root "systemctl daemon-reload"
+ssh ev3_root "systemctl restart ev3.service"
 
 
 echo "Pulling on rpi"
 
-ssh rpi bash -c "cd sixers;git pull"
+ssh rpi "cd sixers;git pull"
 
 echo "restarting systemd service on rpi"
 
-ssh rpi_root bash -c "systemctl daemon-reload"
-ssh rpi_root bash -c "systemctl restart albert.service"
+ssh rpi_root "systemctl daemon-reload"
+ssh rpi_root "systemctl restart albert.service"
