beafb48d01977876b933657e393b3d03a7736d1c
hkdeman <hemangkandwal@gmail.com>
2019-03-29 21:04:22 +0000

    Added server 2

--- main/test_send_commands.py
+++ main/test_send_commands.py
@@ -6,20 +6,17 @@ sys.path.append(os.path.abspath('..'))
 
 from vision.constants import PORT
 
-from vision.server import start_socket, start_threads
+from vision.server2 import Server
 
-start_threads()
-
-GLOBAL_EV3_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-GLOBAL_EV3_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-GLOBAL_EV3_SOCKET.bind(('0.0.0.0', PORT))
-GLOBAL_EV3_SOCKET.listen(1)
-GLOBAL_EV3_CONN, GLOBAL_EV3_ADDRESS = GLOBAL_EV3_SOCKET.accept()
 
 commands = ['LEFT', 'LEFT', 'END']
 
+server = Server()
+
+print("woah")
+sys.exit(0)
 try:
-    start_socket(commands, GLOBAL_EV3_SOCKET, GLOBAL_EV3_CONN, GLOBAL_EV3_ADDRESS, True)
+    server.setup_order(commands, True)
 except Exception as e:
-    GLOBAL_EV3_SOCKET.close()
+    server.crash()
     raise e
