354394fa84762485dfd0470f7bf9c0f109ca9ffa
Visgean Skeloru <me@visgean.me>
2019-03-30 20:13:06 +0000

    back to http :( the proxy does not support it

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ if DEBUG:
 else:
     import sentry_sdk
     sentry_sdk.init("https://1502091405da4d9e9e6682f7caf67052@sentry.io/1408808")
-    API_URL = os.getenv('ALBERT_URL', 'https://albertwaitfor.me/api/')
+    API_URL = os.getenv('ALBERT_URL', 'http://albert.visgean.me/api/')
 
 
 API_CURRENT_PLAN_URL = API_URL + 'plans/latest/'
