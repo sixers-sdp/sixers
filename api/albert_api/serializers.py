018296f961da8a3bc2c247ef65481c1a7ffe206a
Visgean Skeloru <me@visgean.me>
2019-04-01 19:33:07 +0100

    add cancellations views

--- api/albert_api/serializers.py
+++ api/albert_api/serializers.py
@@ -55,6 +55,19 @@ class DotAssociationSerializer(serializers.ModelSerializer):
 
 
 class CancellationSerializer(serializers.ModelSerializer):
+    device_id = serializers.CharField(write_only=True)
+
     class Meta:
         model = Cancellation
         exclude = []
+
+        extra_kwargs = {
+            'table_number': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        device_id = validated_data.pop('device_id')
+        table_num = DotAssociation.objects.get(dot_id=device_id).location
+        validated_data['table_number'] = table_num
+        order = super().create(validated_data)
+        return order
